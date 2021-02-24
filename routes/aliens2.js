305497634df399c461beac7e4cce5e8961dee4e9
PraveenM24 <praveen050800@gmail.com>
2021-02-24 16:01:59 +0530

    Initial commit

--- routes/aliens2.js
+++ routes/aliens2.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const Alien = require('../models/alien2')
+const Alien = require('../models/alien')
 
 router.get('/', async(req, res) => {
     try{
